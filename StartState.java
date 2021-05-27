fe46693dbec509e52cd778791f05af7546d8900f
bullyhunter1917 <45041977+bullyhunter1917@users.noreply.github.com>
2021-05-27 18:55:53 +0200

    finished start screen and finished uml

--- StartState.java
+++ StartState.java
@@ -6,6 +6,8 @@ public class StartState extends JPanel implements ActionListener {
 
     private StateMengaer msg;
     private Image background;
+    private Image play_button;
+    private Image tlo_napisu;
 
     public StartState(StateMengaer msg) {
         this.msg = msg;
@@ -15,23 +17,25 @@ public class StartState extends JPanel implements ActionListener {
     public void initStartState() {
         this.setLayout(null);
 
-        loadImage();
+        background = loadImage("game_background.jpg");
+        play_button = loadImage("play_button.png");
+        tlo_napisu = loadImage("tlo_napisu_start.png");
 
-        int w = background.getWidth(this);
-        int h = background.getHeight(this);
-        setPreferredSize(new Dimension(w, h));
+        setPreferredSize(new Dimension(1920,1080));
         
-        JButton b = new JButton("START");
-        //b.setPreferredSize(new Dimension(100,50));
-        b.setBounds(w/2-200, h/2+200, 400, 100);
+        JButton b = new JButton();
+        b.setBounds(710, 740, 500, 150);
+        b.setOpaque(false);
+        b.setContentAreaFilled(false);
+        b.setBorderPainted(false);
         b.addActionListener(this);
         b.setActionCommand("START");
         add(b);
     }
 
-    public void loadImage() {
-        ImageIcon ii = new ImageIcon("resourses/background_start_screen.jpg");
-        background = ii.getImage();
+    private Image loadImage(String path) {
+        ImageIcon ii = new ImageIcon("resourses/" + path);
+        return ii.getImage();
     }
 
     @Override
@@ -39,6 +43,12 @@ public class StartState extends JPanel implements ActionListener {
         super.paintComponent(g);
         //Drawing background of this state
         g.drawImage(background, 0, 0, null);
+        g.drawImage(tlo_napisu, 590, 80, null);
+        g.setFont(new Font("SigmarOne", Font.PLAIN, 120));
+        g.drawString("Welcome", 730, 180);
+        g.drawString("to", 900, 300);
+        g.drawString("Duck Clicker", 630, 420);
+        g.drawImage(play_button, 710, 740, null);
     }
 
     @Override
